df51c0e1972a440fd01a310b54afa4d0ebd0392c
Benceee999 <bencehello4@gmail.com>
2023-02-22 17:41:44 +0100

    broke login + new files

---
+++ belepes.php
@@ -0,0 +1,16 @@
+<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Bejelentkezés
+        </a>
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+            <form class="px-4 py-3" action="" method="post">
+                <div class="form-group">
+                    <label for="username">Felhasználónév</label>
+                    <input type="text" class="form-control" id="username" name="username" placeholder="Felhasználónév">
+                </div>
+                <div class="form-group">
+                    <label for="password">Jelszó</label>
+                    <input type="password" class="form-control" id="password" name="password" placeholder="Jelszó">
+                </div>
+                <button type="submit" name="submit" class="btn btn-primary">Bejelentkezés</button>
+            </form>
+        </div>
---
+++ db_inc.php
@@ -0,0 +1,47 @@
+<?php
+
+class DataBase {
+
+    /* private $servername = "localhost";
+    private $username = "c31hbencedbu";
+    private $password = "mJaTyh!D52";
+    private $dbname = "c31hbencedb";
+ */
+    private $servername = "localhost";
+    private $username = "Bencee999";
+    private $password = "E(NN(!YKO-tRqSwt";
+    private $dbname = "szakdoga"; 
+
+    private $conn;
+
+    function __construct() {
+        // Create connection
+        $conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        $this->conn = $conn;
+    }
+
+    /**
+     * 
+     */
+    public function dbSelect($sql) {
+        if($result = $this->conn->query($sql)) {
+            if ($result->num_rows > 0) {
+                return $result; 
+              }
+              else {
+                  return NULL;
+              }
+        }
+        elseif($this->conn->error) {
+            die("SQL hiba: " . $this->conn->error);
+        }
+    }
+}
+
+
+?>
